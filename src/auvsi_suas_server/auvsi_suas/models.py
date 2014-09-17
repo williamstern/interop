Added scipy to numpy definition. Updated models to have moving position calculation. Updated views to be more condensed. Added various fixes to style.

src/auvsi_suas_server/auvsi_suas/models.py
@@ -1,39 +1,41 @@
 """Models for the AUVSI SUAS System."""
 
 import datetime
+import numpy as np
+import math
+import time
 from django.conf import settings
 from django.db import models
-from math import asin
-from math import cos
-from math import radians
-from math import sin
-from math import sqrt
 
 
 def haversine(lon1, lat1, lon2, lat2):
-      """
-      Calculate the great circle distance between two points 
-      on the earth (specified in decimal degrees).
-      Reference:
-      http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-      Args:
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees).
+    Reference:
+    http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+    Args:
         lon1, lat1: The latitude and longitude of position 1
         lon2, lat2: The latitude and longitude of position 2
-      Returns:
+    Returns:
         The distance in kilometers
-      """
-      # convert decimal degrees to radians 
-      lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    """
+    # convert decimal degrees to radians
+    lon1 = math.radians(lon1)
+    lat1 = math.radians(lat1)
+    lon2 = math.radians(lon2)
+    lat2 = math.radians(lat2)
 
-      # haversine formula 
-      dlon = lon2 - lon1 
-      dlat = lat2 - lat1 
-      a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-      c = 2 * asin(sqrt(a)) 
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    hav_a = (math.sin(dlat/2)**2
+             + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2)
+    hav_c = 2 * math.asin(math.sqrt(hav_a))
 
-      # 6367 km is the radius of the Earth
-      km = 6367 * c
-      return km
+    # 6367 km is the radius of the Earth
+    dist_km = 6367 * hav_c
+    return dist_km
 
 
 class GpsPosition(models.Model):
@@ -43,17 +45,18 @@ class GpsPosition(models.Model):
     # Longitude in degrees
     longitude = models.FloatField()
 
-    def distanceTo(other):
+    def distanceTo(self, other):
         """Computes distance to another position.
         Args:
           other: The other position.
         Returns:
           Distance in feet.
         """
-        km = haversine(longitude, latitude, other.longitude, other.latitude)
+        dist = haversine(
+                self.longitude, self.latitude, other.longitude, other.latitude)
         # Convert km to feet
-        ft = km * 3280.84
-        return ft
+        dist_ft = dist * 3280.84
+        return dist_ft
 
 
 class AerialPosition(models.Model):
@@ -63,15 +66,15 @@ class AerialPosition(models.Model):
     # MSL altitude in feet
     msl_altitude = models.FloatField()
 
-    def distanceTo(other):
+    def distanceTo(self, other):
         """Computes distance to another position.
         Args:
           other: The other position.
         Returns:
           Distance in feet.
         """
-        return math.hypot(math.abs(ms_altitude - other.msl_altitude),
-                          gps_position.distanceTo(other.gps_position))
+        return math.hypot(abs(self.msl_altitude - other.msl_altitude),
+                          self.gps_position.distanceTo(other.gps_position))
 
 
 class Waypoint(models.Model):
@@ -83,14 +86,14 @@ class Waypoint(models.Model):
     # Waypoint relative order number (optional)
     order = models.IntegerField()
 
-    def distanceTo(other):
+    def distanceTo(self, other):
         """Computes distance to another waypoint.
         Args:
           other: The other waypoint.
         Returns:
           Distance in feet.
         """
-        return position.distanceTo(other.position)
+        return self.position.distanceTo(other.position)
 
 
 class ServerInfo(models.Model):
@@ -100,9 +103,17 @@ class ServerInfo(models.Model):
     # Message for teams
     team_msg = models.CharField(max_length=100)
 
+    def toJSON(self):
+        """Obtains a JSON style python representation for the data."""
+        data = {
+            'message': self.team_msg,
+            'message_timestamp': str(self.timestamp)
+        }
+        return data
+
 
 class ServerInfoAccessLog(models.Model):
-    """Log of access to the ServoInfo objects used to evaluate teams."""
+    """Log of access to the ServerInfo objects used to evaluate teams."""
     # Timestamp of the access
     timestamp = models.DateTimeField(default=datetime.datetime.now)
     # The user which accessed the data
@@ -132,6 +143,18 @@ class StationaryObstacle(Obstacle):
     # The height of the cylinder in feet
     cylinder_height = models.FloatField()
 
+    def toJSON(self):
+        """Obtains a JSON style python representation for the data."""
+        data = {
+            'gps_position': {
+              'latitude':  self.gps_position.latitude,
+              'longitude': self.gps_position.longitude
+            },
+            'cylinder_radius': self.cylinder_radius,
+            'cylinder_height': self.cylinder_height
+        }
+        return data
+
 
 class MovingObstacle(Obstacle):
     """A moving obstacle that teams must avoid."""
@@ -141,6 +164,79 @@ class MovingObstacle(Obstacle):
     speed_max = models.FloatField()
     # The radius of the sphere in feet
     sphere_radius = models.FloatField()
+
+    def getWaypointTravelTime(self, waypoints, id_tm1, id_t):
+        """Gets the travel time to the current waypoint from a previous.
+
+        Args:
+          waypoints: A set of sorted waypoints which define a path.
+          id_tm1: The ID of the starting waypoint.
+          id_t: The ID of the ending waypoint.
+        Returns:
+          Time to travel between the two waypoints in seconds.
+        """
+        waypoint_t = waypoints[id_t]
+        waypoint_tm1 = waypoints[id_tm1]
+        waypoint_dist = waypoint_tm1.distanceTo(waypoint_t)
+        waypoint_travel_time = waypoint_dist / self.speed_max
+        return waypoint_travel_time
+
+
+    def getPosition(self, cur_time=datetime.datetime.now()):
+        """Gets the current position for the obstacle.
+
+        Args:
+          cur_time: The current time as datetime.
+        Returns:
+          Returns a tuple (latitude, longitude, altitude_msl) for the obstacle
+          at the given time. Returns None if could not compute.
+        """
+        # Obtain waypoint positions which define flight path
+        waypoints = self.waypoints.order_by('order')
+        num_waypoints = len(waypoints)
+        positions = np.zeros(num_waypoints + 1, 3)
+        for waypoint_id in range(num_waypoints):
+            cur_waypoint = waypoints[waypoint_id]
+            cur_position = cur_waypoint.position
+            cur_gps_pos = cur_position.gps_position
+            positions[waypoint_id, 0] = cur_gps_pos.latitude
+            positions[waypoint_id, 1] = cur_gps_pos.longitude
+            positions[waypoint_id, 2] = cur_position.msl_altitude
+
+        # Compute times of waypoint positions
+        pos_times = np.zeros(num_waypoints + 1)
+        for waypoint_id in range(1, num_waypoints):
+            # Current intra waypoint travel time
+            cur_travel_time = self.getWaypointTravelTime(
+                waypoints, waypoint_id-1, waypoint_id)
+            cur_time = pos_times[waypoint_id-1] + cur_travel_time
+            pos_times[waypoint_id] = cur_time
+        # Final travel time that closes polygon
+        final_travel_time = self.getWaypointTravelTime(
+            waypoints, num_waypoints-1, 0)
+        final_time = pos_times[num_waypoints-1] + final_travel_time
+        pos_times[num_waypoints] = final_time
+
+        # Use spline interpolation to find current position
+        tck = np.interpolate.splrep(pos_times, positions, per=1)
+        cur_time = np.mod(time.time(), pos_times[num_waypoints])
+        cur_pos = np.interpolate.splev(cur_time, tck)
+        latitude = cur_pos[0]
+        longitude = cur_pos[1]
+        altitude_msl = cur_pos[2]
+
+        return (latitude, longitude, altitude_msl)
+
+    def toJSON(self):
+        """Obtains a JSON style python representation for the data."""
+        (latitude, longitude, altitude_msl) = self.getPosition()
+        data = {
+            'sphere_radius': self.sphere_radius,
+            'latitude': latitude,
+            'longitude': longitude,
+            'altitude_msl': altitude_msl
+        }
+        return data
 
 
 class UasTelemetry(models.Model):

src/auvsi_suas_server/auvsi_suas/views.py
@@ -6,9 +6,14 @@ import time
 import numpy as np
 from auvsi_suas.models import AerialPosition
 from auvsi_suas.models import GpsPosition
+from auvsi_suas.models import MovingObstacle
+from auvsi_suas.models import Obstacle
+from auvsi_suas.models import ObstacleAccessLog
 from auvsi_suas.models import ServerInfo
 from auvsi_suas.models import ServerInfoAccessLog
+from auvsi_suas.models import StationaryObstacle
 from auvsi_suas.models import UasTelemetry
+from auvsi_suas.models import Waypoint
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
@@ -87,10 +92,10 @@ def getServerInfo(request):
         return HttpResponseServerError('No server info available.')
     else:
         # Form JSON response
-        data = dict()
-        data['server_time'] = str(datetime.datetime.now())
-        data['message'] = server_info.team_msg
-        data['message_timestamp'] = str(server_info.timestamp)
+        data = {
+            'server_info': server_info.toJSON(),
+            'server_time': str(datetime.datetime.now())
+        }
         return HttpResponse(json.dumps(data),
                             content_type="application/json")
 
@@ -108,69 +113,23 @@ def getObstacles(request):
     stationary_obstacles = StationaryObstacles.objects.all()
     stationary_obstacles_json = list()
     for cur_obst in stationary_obstacles:
-        cur_obst_json = dict()
-        cur_gps = dict()
-        # Set obstacle position
-        cur_gps['latitude'] = cur_obst.gps_position.latitude
-        cur_gps['longitude'] = cur_obst.gps_position.longitude
-        cur_obst_json['gps_position'] = cur_gps
-        # Set obstacle size
-        cur_obst_json['cylinder_radius'] = cur_obst.cylinder_radius
-        cur_obst_json['cylinder_height'] = cur_obst.cylinder_height
         # Add current obstacle
+        cur_obst_json = cur_obst.toJSON()
         stationary_obstacles_json.append(cur_obst_json)
 
     # Form JSON response portion for moving obstacles
     moving_obstacles = MovingObstacle.objects.all()
     moving_obstacles_json = list()
     for cur_obst in moving_obstacles:
-        cur_obst_json = dict()
-        # Set obstacle size
-        cur_obst_json['sphere_radius'] = cur_obst.sphere_radius
-        # Get obstacle speed
-        speed_max = cur_obst.speed_max
-        if speed_max <= 0:
-            continue
-        # Obtain waypoint positions which define flight path
-        waypoints = cur_obst.waypoints.order_by('order')
-        num_waypoints = len(waypoints)
-        positions = np.zeros(num_waypoints + 1, 3)
-        for waypoint_id in range(num_waypoints):
-            cur_waypoint = waypoints[waypoint_id]
-            cur_position = cur_waypoint.position
-            cur_gps_pos = cur_position.gps_position
-            positions[waypoint_id, 0] = cur_gps_pos.latitude
-            positions[waypoint_id, 1] = cur_gps_pos.longitude
-            positions[waypoint_id, 2] = cur_position.msl_altitude
-        # Compute times of waypoint positions
-        pos_times = np.zeros(num_waypoints + 1)
-        for waypoint_id in range(1, num_waypoints):
-            waypoint_t = waypoints[waypoint_id]
-            waypoint_tm1 = waypoints[waypoint_id-1]
-            waypoint_dist = waypoint_tm1.distanceTo(waypoint_t)
-            waypoint_travel_time = waypoint_dist / speed_max
-            cur_time = pos_times[waypoint_id-1] + waypoint_travel_time
-            pos_times[waypoint_id] = cur_time
-        first_point = waypoints[0]
-        final_point = waypoints[num_waypoints-1]
-        final_dist = final_point.distanceTo(first_point)
-        final_travel_time = final_dist / speed_max
-        final_time = pos_times[num_waypoints-1] + final_travel_time
-        pos_times[num_waypoints] = final_time
-        # Use spline interpolation to find current position
-        tck = interpolate.splrep(pos_times, positions, per=1)
-        cur_time = np.mod(time.time(), pos_times[num_waypoints])
-        cur_pos = interpolate.splev(cur_time, tck)
-        cur_obst_json['latitude'] = cur_pos[0]
-        cur_obst_json['longitude'] = cur_pos[1]
-        cur_obst_json['altitude_msl'] = cur_pos[2]
         # Add current obstacle
+        cur_obst_json = cur_obst.toJSON()
         moving_obstacles_json.append(cur_obst_json)
 
     # Form final JSON response
-    data = dict()
-    data['stationary_obstacles'] = stationary_obstacles_json
-    data['moving_obstacles'] = moving_obstacles_json
+    data = {
+        'stationary_obstacles': stationary_obstacles_json,
+        'moving_obstacles': moving_obstacles_json
+    }
     return HttpResponse(json.dumps(data),
                         content_type="application/json")
 