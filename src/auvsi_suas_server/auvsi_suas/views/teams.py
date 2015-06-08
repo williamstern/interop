7e4590ad65684aa6619306a17c7e0f12b0e395d0
Michael Pratt <michael@pratt.im>
2015-06-07 22:52:39 -0700

    Add /api/teams/{id}/ endpoint
    Per #29, add the `/api/teams/{id}/` endpoint, implementing the `GET` and
    `PUT` methods.
    
    This allows adding TakeoffOrLandingEvents for users with a `PUT`
    request.

--- src/auvsi_suas_server/auvsi_suas/views/teams.py
+++ src/auvsi_suas_server/auvsi_suas/views/teams.py
@@ -8,6 +8,15 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 
+def userJson(user):
+    """Generate JSON-style dict for user."""
+    return {
+        'name': user.username,
+        'id': user.pk,
+        'in_air':  TakeoffOrLandingEvent.userInAir(user),
+        'active':  UasTelemetry.userActive(user),
+    }
+
 # Require admin access
 @user_passes_test(lambda u: u.is_superuser)
 def getTeams(request):
@@ -24,12 +33,46 @@ def getTeams(request):
     for user in users:
         # Only standard users are exported
         if not user.is_superuser:
-            teams.append({
-                'name': user.username,
-                'id': user.pk,
-                'in_air':  TakeoffOrLandingEvent.userInAir(user),
-                'active':  UasTelemetry.userActive(user),
-            })
+            teams.append(userJson(user))
 
     return HttpResponse(json.dumps(teams),
+                        content_type="application/json")
+
+# Require admin access
+@user_passes_test(lambda u: u.is_superuser)
+def getTeamsId(request, pk):
+    """GET/PUT specific team."""
+    # Only GET/PUT requests
+    if request.method not in ['GET', 'PUT']:
+        logger.warning('Invalid request method for teams request.')
+        logger.debug(request)
+        return HttpResponseBadRequest('Request must be GET or PUT request.')
+
+    try:
+        user = User.objects.get(pk=int(pk))
+    except User.DoesNotExist:
+        return HttpResponseBadRequest('Unknown team %s' % pk)
+
+    # PUT allows updating in-air status
+    if request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+        except ValueError:
+            return HttpResponseBadRequest('Invalid JSON: %s' % request.body)
+
+        # We ignore everything except 'in_air'
+        if 'in_air' in data:
+            in_air = data['in_air']
+
+            if in_air is not True and in_air is not False:
+                return HttpResponseBadRequest('in_air must be boolean')
+
+            currently_in_air = TakeoffOrLandingEvent.userInAir(user)
+
+            # New event only necessary if changing status
+            if currently_in_air != in_air:
+                event = TakeoffOrLandingEvent(user=user, uas_in_air=in_air)
+                event.save()
+
+    return HttpResponse(json.dumps(userJson(user)),
                         content_type="application/json")
