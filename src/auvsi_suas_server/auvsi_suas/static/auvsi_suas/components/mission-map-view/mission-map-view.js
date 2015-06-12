484a6a4e4bff738620282189d5f2406b36e4a0b8
Phillip Tischler <pmtischler@gmail.com>
2015-06-12 00:51:11 -0400

    Updated view to auto-resize to screen. Added camera controls.

--- src/auvsi_suas_server/auvsi_suas/static/auvsi_suas/components/mission-map-view/mission-map-view.js
+++ src/auvsi_suas_server/auvsi_suas/static/auvsi_suas/components/mission-map-view/mission-map-view.js
@@ -6,16 +6,75 @@
 /**
  * Directive for 3D map view of Mission Scene.
  */
-MissionMapView = function(MissionScene) {
-    // Return the parameters for the directive.
-    return {
-        restrict: 'E',
-        scope: {
-            missionScene: '=',
-        },
-        templateUrl: '/static/auvsi_suas/components/mission-map-view/mission-map-view.html',
-        link: MissionMapViewLinkElement
-    };
+MissionMapView = function($window, MissionScene) {
+    /**
+     * The window service.
+     */
+    this.window_ = $window;
+
+    /**
+     * The mission scene service.
+     */
+    this.missionScene_ = MissionScene;
+
+    /**
+     * The element scope.
+     */
+    this.scope_ = null;
+
+    /**
+     * The element.  */
+    this.element_ = null;
+
+    /**
+     * The element attributes.
+     */
+    this.attrs_ = null;
+
+    /**
+     * The width of the view.
+     */
+    this.width_ = null;
+
+    /**
+     * The height of the view.
+     */
+    this.height_ = null;
+
+    /**
+     * The WebGL renderer.
+     */
+    this.renderer_ = null;
+
+    /**
+     * The camera.
+     */
+    this.camera_ = null;
+
+    /**
+     * Raycaster for view.
+     */
+    this.raycaster_ = new THREE.Raycaster();
+
+    /**
+     * The mouse down position.
+     */
+    this.mouseDownPos_ = new THREE.Vector2();
+
+    /**
+     * The mouse move position.
+     */
+    this.mouseMovePos_ = new THREE.Vector2();
+
+    /**
+     * The mouse down position in world coordinates.
+     */
+    this.mouseDownWorldPos_ = new THREE.Vector3();
+
+    /**
+     * The mouse move position in world coordinates.
+     */
+    this.mouseMoveWorldPos_ = new THREE.Vector3();
 };
 
 
@@ -25,41 +84,181 @@ MissionMapView = function(MissionScene) {
  * @param element The directive element.
  * @param attrs The directive attributes.
  */
-MissionMapViewLinkElement = function(scope, element, attrs) {
-    // TODO(pmtischler): Make actual camera.
-    // Create a unique camera.
-    // TODO(pmtischler): Update size based on element size.
-    var width = 500; // element.innerWidth
-    var height = 500;
-    var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.z = 5;
+MissionMapView.prototype.link = function(scope, element, attrs) {
+    // Store directive information.
+    this.scope_ = scope;
+    this.element_ = element;
+    this.attrs_ = attrs;
 
-    // Create a unqiue renderer and add to directive element.
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-    element.append(renderer.domElement);
+    // Create a renderer and add to directive element.
+    this.renderer_ = new THREE.WebGLRenderer({antialias: true});
+    element.append(this.renderer_.domElement);
+    // Create a camera and configure it.
+    var fieldOfView = 60;
+    var aspectRatio = 1;
+    var nearClipPlane = 0.1;
+    var farClipPlane = 100000;
+    this.camera_ = new THREE.PerspectiveCamera(
+            fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
 
+    // Set camera and renderer sizes.
+    this.setCameraAndRendererSize_();
+
+    // Start the camera above the ground, with 45deg azimuth, looking at origin.
+    this.camera_.position.set(0, -300, 300);
+    this.camera_.up = new THREE.Vector3(0, 0, 1);
+    this.camera_.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // Whenever the window resizes, update the camera and renderer.
+    angular.element(this.window_).on(
+            'resize', angular.bind(this, this.setCameraAndRendererSize_));
+
+    // Whenever mouse is pressed/moved/scrolled, update camera.
+    angular.element(element).on(
+            'mousedown', angular.bind(this, this.mouseUpDown_, true));
+    angular.element(element).on(
+            'mouseup', angular.bind(this, this.mouseUpDown_, false));
+    angular.element(element).on(
+            'mousemove', angular.bind(this, this.mouseMoved_));
+    angular.element(element).on(
+            'mousewheel', angular.bind(this, this.mouseScrolled_));
+    
     // Start render loop.    
-    MissionMapViewRender(scope, camera, renderer);
+    this.render_();
+};
+
+
+/**
+ * Sets the camera and renderer to match the view.
+ */
+MissionMapView.prototype.setCameraAndRendererSize_ = function() {
+    this.width_ = this.element_[0].offsetWidth - this.scope_.offsetWidth;
+    this.height_ = this.window_.innerHeight - this.scope_.offsetHeight;
+    // Set renderer size.
+    this.renderer_.setSize(this.width_, this.height_);
+    // Set camera aspect ratio.
+    this.camera_.aspect = this.width_ / this.height_;
+    this.camera_.updateProjectionMatrix();
+};
+
+
+/**
+ * Gets the normalized device coordinates for the position.
+ * @param offsetX The screen X coordinate.
+ * @param offsetY The screen Y coordinate.
+ * @return The normalized device coordinates.
+ */
+MissionMapView.prototype.getNormalizedDeviceCoords_ = function(
+        offsetX, offsetY, pos) {
+    var pos = new THREE.Vector2();
+    pos.x = (event.offsetX / this.width_) * 2 - 1;
+    pos.y = (event.offsetY / this.height_) * -2 + 1;
+    return pos;
+};
+
+
+/**
+ * Gets the mouse world position for a click on the ground.
+ * @param mousePos The mouse position.
+ * @return The mouse world position, or null if no intersection was found.
+ */
+MissionMapView.prototype.getMousePositionOnGround_ = function(
+        mousePos, mouseWorldPos) {
+    this.raycaster_.setFromCamera(mousePos, this.camera_);
+    var intersects = this.raycaster_.intersectObject(this.missionScene_.ground);
+    if (intersects.length == 0) {
+        return null;
+    }
+    return intersects[0].point;
+};
+
+
+/**
+ * Updates the state of the mouse click.
+ * @param mouseDown Whether the mouse is down.
+ */
+MissionMapView.prototype.mouseUpDown_ = function(mouseDown, event) {
+    this.mouseDown_ = mouseDown;
+
+    if(mouseDown) {
+        // Track the mouse down position in world coordinates.
+        this.mouseDownPos_ = this.getNormalizedDeviceCoords_(
+                event.offsetX, event.offsetY);
+        this.mouseDownWorldPos_ = this.getMousePositionOnGround_(
+                this.mouseDownPos_);
+    }
+};
+
+
+/**
+ * Updates the mouse position and camera.
+ * @param event The event containing mouse details.
+ */
+MissionMapView.prototype.mouseMoved_ = function(event) {
+    event.preventDefault();
+    // If mouse is not down, ignore.
+    if (!this.mouseDown_) {
+        return;
+    }
+
+    // Get mouse position in world coordinates to compare against mouse down.
+    this.mouseMovePos_ = this.getNormalizedDeviceCoords_(
+            event.offsetX, event.offsetY);
+    this.mouseMoveWorldPos_ = this.getMousePositionOnGround_(
+            this.mouseMovePos_);
+
+    // Apply offset to move position under mouse back to position at mouse down.
+    if (!!this.mouseDownWorldPos_ && !!this.mouseMoveWorldPos_) {
+        var mouseWorldMovement = new THREE.Vector3();
+        mouseWorldMovement.subVectors(this.mouseDownWorldPos_, this.mouseMoveWorldPos_);
+        this.camera_.position.x += mouseWorldMovement.x;
+        this.camera_.position.y += mouseWorldMovement.y;
+        this.camera_.updateMatrixWorld();
+    }
+};
+
+
+/**
+ * Updates the mouse scroll and camera.
+ * @param event The event containing mouse details.
+ */
+MissionMapView.prototype.mouseScrolled_ = function(event) {
+    this.camera_.position.z += event.originalEvent.deltaY;
+    if (this.camera_.position.z < 1) {
+        this.camera_.position.z = 1;
+    }
+    if (this.camera_.position.z > 10000) {
+        this.camera_.position.z = 10000;
+    }
 };
 
 
 /**
  * Renders the mission scene in a render loop.
- * @param scope The scope containing the MissionScene.
- * @param camera The camera to render with.
- * @param renderer The renderer to render with.
  */
-MissionMapViewRender = function(scope, camera, renderer) {
-    requestAnimationFrame(
-            angular.bind(this, MissionMapViewRender, scope, camera, renderer));
-    if (!!scope.missionScene.scene) {
-        renderer.render(scope.missionScene.scene, camera);
+MissionMapView.prototype.render_ = function() {
+    requestAnimationFrame(angular.bind(this, this.render_));
+
+    if (!!this.missionScene_.scene) {
+        this.renderer_.render(this.missionScene_.scene, this.camera_);
     }
 };
 
 
 // Register the directive.
 angular.module('auvsiSuasApp').directive('missionMapView', [
-    MissionMapView
+    '$window',
+    'MissionScene',
+    function($window, MissionScene) {
+        var mapView = new MissionMapView($window, MissionScene);
+        return {
+            restrict: 'E',
+            scope: {
+                offsetWidth: '=',
+                offsetHeight: '='
+            },
+            templateUrl: '/static/auvsi_suas/components/mission-map-view/mission-map-view.html',
+            link: angular.bind(mapView, mapView.link)
+        };
+    }
 ]);
--- src/auvsi_suas_server/auvsi_suas/static/auvsi_suas/components/mission-scene-service/mission-scene-service.js
+++ src/auvsi_suas_server/auvsi_suas/static/auvsi_suas/components/mission-scene-service/mission-scene-service.js
@@ -11,14 +11,29 @@
  */
 MissionScene = function($rootScope) {
     /**
+     * The root scope service.
+     */
+    this.rootScope_ = $rootScope;
+
+    /**
      * The scene that is built by the service.
      */
     this.scene = null;
 
     /**
-     * The root scope service.
+     * The ground plane.
      */
-    this.rootScope_ = $rootScope;
+    this.groundTexture = THREE.ImageUtils.loadTexture(
+            '/static/auvsi_suas/components/mission-scene-service/img/ground.jpg');
+    this.groundTexture.wrapS = THREE.RepeatWrapping;
+    this.groundTexture.wrapT = THREE.RepeatWrapping;
+    this.groundTexture.magFilter = THREE.LinearFilter;
+    this.groundTexture.minFilter = THREE.LinearFilter;
+    this.groundTexture.repeat.set(1000, 1000);
+    this.groundMaterial = new THREE.MeshBasicMaterial({color: 0x526f35, map: this.groundTexture});
+    this.groundGeometry = new THREE.PlaneBufferGeometry(1, 1);
+    this.ground = new THREE.Mesh(this.groundGeometry, this.groundMaterial);
+    this.ground.scale.set(100000, 100000, 100000);
 };
 
 
@@ -31,10 +46,15 @@ MissionScene = function($rootScope) {
 MissionScene.prototype.rebuildScene = function(mission, obstacles, telemetry) {
     // Create fresh scene for rebuild.
     var scene = new THREE.Scene();
+
+    // Add the ground plane.
+    scene.add(this.ground);
+
     // TODO(pmtischler): Make a real scene with the data.
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
+    cube.scale.set(10,10,10);
     scene.add( cube );
 
     // Update the scene and notify others.
