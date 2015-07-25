2ec0e10e9c3909fb436d16f7abb75a783258a6bf
Phillip Tischler <pmtischler@gmail.com>
2015-07-25 18:50:26 -0400

    Install Karma and Jasmine via NPM.

--- setup/puppet_files/auvsi_suas/manifests/server_install.pp
+++ setup/puppet_files/auvsi_suas/manifests/server_install.pp
@@ -5,17 +5,25 @@
 class auvsi_suas::server_install {
     require auvsi_suas::base
 
-    # Install some packages from apt, since they are slow/tricky to build.
+    # Install packages from apt.
     $package_deps = [
         "python-numpy",
         "python-scipy",
         "python-matplotlib",
         "python-psycopg2",
+        "npm",
     ]
     package { $package_deps:
         ensure => "latest",
     }
 
+    # Install python server requirements.
     python::requirements { '/interop/server/requirements.txt' :
+    }
+
+    # Install packages from NPM.
+    exec { 'npm install karma and jasmine':
+        command => "npm install -g karma karma-jasmine karma-chrome-launcher karma-cli",
+        cwd => "/interop/server",
     }
 }
