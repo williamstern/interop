3d9ff1732ba064eb74d934234f1e44f4837ef202
Michael Pratt <michael@pratt.im>
2015-08-02 12:24:50 -0700

    client: get obstacles
    Provide the get_obstacles method, which accesses the obstacles endpoint
    and returns lists of StationaryObstacle and MovingObstacle objects.

--- client/new/interop/test_client.py
+++ client/new/interop/test_client.py
@@ -92,3 +92,23 @@ class TestClient(unittest.TestCase):
             # We only accept Telemetry objects (or objects that behave like
             # Telemetry, not dicts.
             self.client.post_telemetry(t)
+
+    def test_get_obstacles(self):
+        """Test getting obstacles."""
+        stationary, moving = self.client.get_obstacles()
+
+        # No exceptions is a good sign, let's see if the data matches the fixture.
+        self.assertEqual(2, len(stationary))
+        self.assertEqual(1, len(moving))
+
+        # Lat, lon, and altitude of the moving obstacles change, so we don't
+        # check those.
+        self.assertEqual(50, moving[0].sphere_radius)
+
+        radii = [o.cylinder_radius for o in stationary]
+        self.assertIn(50, radii)
+        self.assertIn(150, radii)
+
+        heights = [o.cylinder_height for o in stationary]
+        self.assertIn(300, heights)
+        self.assertIn(200, heights)
--- client/new/interop/types.py
+++ client/new/interop/types.py
@@ -8,6 +8,26 @@ values will be accepted by the server.
 import collections
 
 
+def check_latitude(lat):
+    """Assert that latitude is valid.
+
+    Raises:
+        ValueError: Latitude out of range
+    """
+    if lat < -90 or lat > 90:
+        raise ValueError("Latitude (%f) out of range [-90 - 90]" % lat)
+
+
+def check_longitude(lon):
+    """Assert that longitude is valid.
+
+    Raises:
+        ValueError: Longitude out of range
+    """
+    if lon < -180 or lon > 180:
+        raise ValueError("Longitude (%f) out of range [-180, 180]" % lon)
+
+
 class Serializable(object):
     """ Serializable is a simple base class which provides basic
     'serialization' (a simple dict) of a subset of attributes the inheriting
@@ -53,13 +73,8 @@ class Telemetry(Serializable):
         self.altitude_msl = float(altitude_msl)
         self.uas_heading = float(uas_heading)
 
-        if self.latitude < -90 or self.latitude > 90:
-            raise ValueError("Latitude (%f) out of range [-90 - 90]" %
-                             self.latitude)
-
-        if self.longitude < -180 or self.longitude > 180:
-            raise ValueError("Longitude (%f) out of range [-180, 180]" %
-                             self.longitude)
+        check_latitude(self.latitude)
+        check_longitude(self.longitude)
 
         if self.uas_heading < 0 or self.uas_heading > 360:
             raise ValueError("Heading (%f) out of range [0, 360]" %
@@ -76,3 +91,71 @@ class ServerInfo(collections.namedtuple(
         server_time: Current server time
     """
     pass
+
+
+class StationaryObstacle(Serializable):
+    """A stationary obstacle.
+
+    This obstacle is a cylinder with a given location, height, and radius.
+
+    Attributes:
+        latitude: Latitude of the center of the cylinder in decimal degrees
+        longitude: Longitude of the center of the cylinder in decimal degrees
+        cylinder_radius: Radius in feet
+        cylinder_height: Height in feet
+
+    Raises:
+        ValueError: Argument not convertable to float or out of range.
+    """
+
+    def __init__(self, latitude, longitude, cylinder_radius, cylinder_height):
+        super(StationaryObstacle, self).__init__(
+            ["latitude", "longitude", "cylinder_radius", "cylinder_height"])
+
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+        self.cylinder_radius = float(cylinder_radius)
+        self.cylinder_height = float(cylinder_height)
+
+        check_latitude(self.latitude)
+        check_longitude(self.longitude)
+
+        if self.cylinder_radius < 0:
+            raise ValueError("Cylinder radius (%f) must be non-negative" %
+                             self.cylinder_radius)
+
+        if self.cylinder_height < 0:
+            raise ValueError("Cylinder height (%f) must be non-negative" %
+                             self.cylinder_height)
+
+
+class MovingObstacle(Serializable):
+    """A moving obstacle.
+
+    This obstacle is a sphere with a given location, altitude, and radius.
+
+    Attributes:
+        latitude: Latitude of the center of the cylinder in decimal degrees
+        longitude: Longitude of the center of the cylinder in decimal degrees
+        altitude_msl: Sphere centroid altitude MSL in feet
+        sphere_radius: Radius in feet
+
+    Raises:
+        ValueError: Argument not convertable to float or out of range.
+    """
+
+    def __init__(self, latitude, longitude, altitude_msl, sphere_radius):
+        super(MovingObstacle, self).__init__(["latitude", "longitude",
+                                              "altitude_msl", "sphere_radius"])
+
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+        self.altitude_msl = float(altitude_msl)
+        self.sphere_radius = float(sphere_radius)
+
+        check_latitude(self.latitude)
+        check_longitude(self.longitude)
+
+        if self.sphere_radius < 0:
+            raise ValueError("Sphere radius (%f) must be non-negative" %
+                             self.sphere_radius)
