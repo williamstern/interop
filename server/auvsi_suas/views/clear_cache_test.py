6c6eaed808231ee455d5425c51b3359bf10ccf5e
Phillip Tischler <pmtischler@gmail.com>
2015-08-16 14:03:20 -0400

    Invalidate cache on model update.

--- server/auvsi_suas/views/clear_cache_test.py
+++ server/auvsi_suas/views/clear_cache_test.py
@@ -4,6 +4,7 @@ import datetime
 from auvsi_suas.models import GpsPosition
 from auvsi_suas.models import MissionConfig
 from auvsi_suas.models import ServerInfo
+from auvsi_suas.models import StationaryObstacle
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -18,6 +19,7 @@ class TestClearCache(TestCase):
         pos.latitude = 10
         pos.longitude = 10
         pos.save()
+        self.pos = pos
 
         info = ServerInfo()
         info.timestamp = datetime.datetime.now()
@@ -38,8 +40,15 @@ class TestClearCache(TestCase):
         config.save()
         self.config = config
 
+        obst = StationaryObstacle()
+        obst.gps_position = pos
+        obst.cylinder_radius = 10
+        obst.cylinder_height = 10
+        obst.save()
+        self.obst = obst
+
         self.login_url = reverse('auvsi_suas:login')
-        self.info_url = reverse('auvsi_suas:server_info')
+        self.obst_url = reverse('auvsi_suas:obstacles')
         self.clear_cache_url = reverse('auvsi_suas:clear_cache')
 
     def test_clear_nonadmin(self):
@@ -69,17 +78,18 @@ class TestClearCache(TestCase):
         })
         self.assertEqual(200, response.status_code)
         # Execute a cached view.
-        response = self.client.get(self.info_url)
+        response = self.client.get(self.obst_url)
         self.assertEqual(response.status_code, 200)
-        # Hide the underlying value for the view. It would return an error if
-        # it wasn't cached. Verify it doesn't return an error because it is
-        # cached.
-        self.config.is_active = False
-        self.config.save()
-        response = self.client.get(self.info_url)
-        self.assertEqual(response.status_code, 200)
-        # Clear the cache and test view now returns an error.
+        # Change the underlying value.
+        self.pos.latitude = 100
+        self.pos.save()
+        # Get the cached value.
+        cached_response = self.client.get(self.obst_url)
+        self.assertEqual(cached_response.status_code, 200)
+        self.assertEqual(response.content, cached_response.content)
+        # Clear the cache and test view now shows updated value.
         response = self.client.get(self.clear_cache_url)
         self.assertEqual(response.status_code, 200)
-        response = self.client.get(self.info_url)
-        self.assertGreaterEqual(response.status_code, 500)
+        response = self.client.get(self.obst_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.content, cached_response.content)
