406679aab14f32dc7181dcd73fcfa905896ddfe2
Michael Pratt <michael@pratt.im>
2015-08-24 21:31:43 -0700

    Install and configure xsendfile
    This will allow Django to use the X-Sendfile header to tell Apache to
    serve target images to authenticated users.

--- setup/puppet_files/auvsi_suas/manifests/apache_setup.pp
+++ setup/puppet_files/auvsi_suas/manifests/apache_setup.pp
@@ -19,36 +19,55 @@ class auvsi_suas::apache_setup {
 
     # Configure apache defaults
     class { 'apache':
-      default_vhost => false,
-      service_ensure => running,
+        default_vhost => false,
+        service_ensure => running,
     }
+
     # Configure the python path
     class { 'apache::mod::wsgi':
-      wsgi_python_home   => '/interop/server/venv/',
-      wsgi_python_path   => '/interop/server/:/interop/server/venv/lib/python2.7/site-packages/',
+        wsgi_python_home   => '/interop/server/venv/',
+        wsgi_python_path   => '/interop/server/:/interop/server/venv/lib/python2.7/site-packages/',
     }
+
+    # xsendfile will be used to serve uploaded files
+    apache::mod { 'xsendfile': }
+
+    # xsendfile configuration
+    file { '/etc/apache2/conf.d/xsendfile.conf' :
+        content => "XSendFile On\nXSendFilePath /var/www/media",
+    }
+
     # Configure production via WSGI
     apache::vhost { 'interop_server':
-      port => '80',
-      docroot => '/var/www/',
-      wsgi_application_group => '%{GLOBAL}',
-      wsgi_daemon_process => 'wsgi',
-      wsgi_daemon_process_options => {
-        processes => '4',
-        threads => '6',
-        display-name => '%{GROUP}',
-      },
-      wsgi_import_script => '/interop/server/server/wsgi.py',
-      wsgi_import_script_options  =>
-        { process-group => 'wsgi', application-group => '%{GLOBAL}' },
-      wsgi_process_group => 'wsgi',
-      wsgi_script_aliases => {
-        '/' => '/interop/server/server/wsgi.py'
-      },
-      aliases => [
-        { alias => '/static',
-          path => '/interop/server/auvsi_suas/static',
+        port => '80',
+        docroot => '/var/www/',
+        wsgi_application_group => '%{GLOBAL}',
+        wsgi_daemon_process => 'wsgi',
+        wsgi_daemon_process_options => {
+            processes => '4',
+            threads => '6',
+            display-name => '%{GROUP}',
         },
-      ],
+        wsgi_import_script => '/interop/server/server/wsgi.py',
+        wsgi_import_script_options  =>
+            { process-group => 'wsgi', application-group => '%{GLOBAL}' },
+        wsgi_process_group => 'wsgi',
+        wsgi_script_aliases => {
+            '/' => '/interop/server/server/wsgi.py'
+        },
+        aliases => [
+            { alias => '/static',
+              path => '/interop/server/auvsi_suas/static',
+            },
+        ],
+        additional_includes => [ '/etc/apache2/conf.d/xsendfile.conf', ],
+        require => File['/etc/apache2/conf.d/xsendfile.conf'],
+    }
+
+    # Ensure media directory exists
+    file { '/var/www/media' :
+        ensure => 'directory',
+        owner => 'www-data',
+        mode => '0644',
     }
 }
