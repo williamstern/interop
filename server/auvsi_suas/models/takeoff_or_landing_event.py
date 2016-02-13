b596822e03986f41635e40a17d1ed0b28b48ae58
Phillip Tischler <pmtischler@gmail.com>
2016-02-13 16:48:55 -0500

    Refactor time periods from flights. Add time periods to clock.

--- server/auvsi_suas/models/takeoff_or_landing_event.py
+++ server/auvsi_suas/models/takeoff_or_landing_event.py
@@ -22,43 +22,15 @@ class TakeoffOrLandingEvent(AccessLog):
     def flights(cls, user):
         """Gets the time periods for which the given user was in flight.
 
-        Duplicate takeoff or landing events are ignored.
-
         Args:
             user: The user for which to get flight periods for.
         Returns:
             A list of TimePeriod objects corresponding to individual flights.
         """
-        # Get the access logs for the user
-        access_logs = TakeoffOrLandingEvent.by_user(user)
-
-        time_periods = []
-
-        # If UAS landing at start, assume forgot to log takeoff, assign infinity
-        if len(access_logs) > 0 and not access_logs[0].uas_in_air:
-            time_periods.append(TimePeriod(None, access_logs[0].timestamp))
-
-        # Use transition from ground to air and air to ground for flight periods
-        takeoff_time = None
-        landing_time = None
-        uas_in_air = False
-        for log in access_logs:
-            # Check for transition from ground to air
-            if not uas_in_air and log.uas_in_air:
-                takeoff_time = log.timestamp
-                uas_in_air = log.uas_in_air
-            # Check for transition from air to ground
-            if uas_in_air and not log.uas_in_air:
-                landing_time = log.timestamp
-                uas_in_air = log.uas_in_air
-
-                time_periods.append(TimePeriod(takeoff_time, landing_time))
-
-        # If UAS in air at end, assume forgot to log landing, assign infinity
-        if uas_in_air:
-            time_periods.append(TimePeriod(access_logs.last().timestamp, None))
-
-        return time_periods
+        return TimePeriod.from_events(
+            TakeoffOrLandingEvent.by_user(user),
+            is_start_func=lambda x: x.uas_in_air,
+            is_end_func=lambda x: not x.uas_in_air)
 
     @classmethod
     def user_in_air(cls, user, time=None):
