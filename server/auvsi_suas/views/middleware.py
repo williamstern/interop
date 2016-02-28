c5d322813e2bca6c1465974bf2c45cd14643778b
Phillip Tischler <pmtischler@gmail.com>
2016-02-28 18:11:11 -0500

    Fix middleware for non-processed requests.

--- server/auvsi_suas/views/middleware.py
+++ server/auvsi_suas/views/middleware.py
@@ -12,7 +12,9 @@ class LoggingMiddleware(object):
         return None
 
     def process_response(self, request, response):
-        delta_time = time.time() - request.start_time
+        delta_time = 'Unknown Runtime'
+        if hasattr(request, 'start_time'):
+            delta_time = '%.4fs' % (time.time() - request.start_time)
 
         req_logger = None
         if response.status_code < 400:
@@ -22,7 +24,7 @@ class LoggingMiddleware(object):
         else:
             req_logger = logger.error
 
-        req_logger('[%d] %s (%.4fs)\n%s\n===\n%s', response.status_code,
+        req_logger('[%d] %s (%s)\n%s\n===\n%s', response.status_code,
                    request.get_full_path(), delta_time, str(request),
                    str(response))
         return response
