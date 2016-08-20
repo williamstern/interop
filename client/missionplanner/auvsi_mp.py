e7eaf589c2186af8da1ac9993eb89371af3ed6c0
Phillip Tischler <pmtischler@gmail.com>
2016-08-20 17:14:16 -0400

    Improve mission planner script provided to teams.
    This only sends telemetry when it changes, which prevents uploading
    duplicate data. It prints error messages from failed requests. It prints
    the upload rate over the last 10 seconds.

--- client/missionplanner/auvsi_mp.py
+++ client/missionplanner/auvsi_mp.py
@@ -1,29 +1,60 @@
+# Add paths to libraries that script needs.
 import sys
 sys.path.append('c:\python27\lib')
-import xmlrpclib
+# Add the path to the interoperability libraries here!
+
+# Configure the script.
+INTEOR_URL = 'http://127.0.0.1'
+INTEROP_USERNAME = 'testuser'
+INTEROP_PASSWORD = 'testpass'
+POLL_SEC = 0.1
+PRINT_SEC = 10
+
+import datetime
+import threading
 from time import time
 
-server = xmlrpclib.ServerProxy('http://127.0.0.1:9000')
-print 'Server Info: {}'.format(server.server_info())
+try:
+    from interop import AsyncClient
+    from interop import Telemetry
+except ImportError as e:
+    raise ImportError(
+        'Failed to import interop libraries. Have you added the libs to '
+        'the path? Error: %s' % e)
+
+# Tracks the uploads since last print.
+sent_lock = threading.Lock()
+last_print = datetime.datetime.now()
+sent_since_print = 0
 
 
-def timing(rate):
-    """
-    Timing Generator, creates delays to achieve the given loop frequency
-    Args:
-        rate: Rate in Hertz
-    """
-    next_time = time()
-    while True:
-        next_time += 1.0 / rate
-        delay = int((next_time - time()) * 1000)
-        if delay > 0:
-            Script.Sleep(delay)
-        yield
+# Tracks upload success and prints any exceptions.
+def handle_upload_result(future):
+    if future.exception():
+        print 'Request Failed. Exception: %s' % str(future.exception())
+    else:
+        with sent_lock:
+            sent_since_print += 1
 
-
+# Continuously poll for new telemetry and send to server.
+last_telemetry = Telemetry(0, 0, 0, 0)
 while True:
-    for _ in timing(rate=12):
-        server.telemetry(
-            float(cs.lat), float(cs.lng), float(cs.alt),
-            float(cs.groundcourse))
+    telemetry = Telemetry(latitude=cs.lat,
+                          longitude=cs.lng,
+                          altitude=cs.alt,
+                          uas_heading=cs.groundcourse)
+    if telemetry != last_telemetry:
+        client.post_telemetry(telemetry).add_done_callback(
+            handle_upload_result)
+        last_telemetry = telemetry
+
+    now = datetime.datetime.now()
+    since_print = (now - last_print).total_seconds()
+    if since_print >= PRINT_SEC:
+        with sent_lock:
+            local_sent_since_print = sent_since_print
+            sent_since_print = 0
+        print 'Upload Rate: %f' % (local_sent_since_print / since_print)
+        last_print = now
+
+    time.sleep(POLL_SEC)
--- docs/integration/mission_planner.rst
+++ docs/integration/mission_planner.rst
@@ -12,39 +12,15 @@ interoperability client library.
 This sample requires that the interoperability client ``interop`` module and
 its dependencies are installed on the system and available in ``PYTHONPATH``.
 
-There are two components to the Mission Planner integration:
-
-`clientproxy.py
-<https://github.com/auvsi-suas/interop/blob/master/client/missionplanner/clientproxy.py>`__
-runs outside of Mission Planner and uses the interoperability client to send
-data it receives from Mission Planner to the interoperability server.
-
 `auvsi_mp.py
 <https://github.com/auvsi-suas/interop/blob/master/client/missionplanner/auvsi_mp.py>`__
-is run as a script inside of Mission Planner. It occasionally sends aircraft
-telemetry to the client proxy, which in turn sends it to the interoperability
-server.
-
-Sample Invocation
------------------
-
-First, run ``clientproxy.py`` in a command prompt outside of Mission Planner,
-passing it the URL of the interoperability server, your username, and your
-password. (See ``clientproxy.py -h`` for invocation details)
-
-.. code-block:: none
-
-    > python clientproxy.py --url "http://192.168.1.5:8080" --username testuser --password testpass
-    Use Control-C to exit
-
-.. note::
-
-    Verify that the URL you specify is accessible from your machine by
-    attempting to open it in a web browser.
+is run as a script inside of Mission Planner. It polls for a change in aircraft
+telemetry, and on change sends the telemetry to the interoperability server. It
+also prints out errors detected and the average upload rate over the last 10
+seconds.
 
 Inside Mission Planner, open the script console (under Actions in the Flight
-Data panel) and paste the contents of ``auvsi_mp.py`` into the window. When you
-close the window, the script will immediately start executing. It will print
-the server info and then immediately begin communicating with the client proxy.
-The client proxy will send telemetry to the interoperability server, and print
-the rate at which it is doing so.
+Data panel) and paste the contents of ``auvsi_mp.py`` into the window. Add a
+line to add the interoperability client libraries to the system path. Modify
+the script constants to provide settings like the interop server URL. When you
+close the window, the script will immediately start executing.
