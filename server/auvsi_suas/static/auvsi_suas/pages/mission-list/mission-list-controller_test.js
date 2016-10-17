da36d5ebf7cb0d10c1d2e8e6895dc26ef4a3a1a4
Phillip Tischler <pmtischler@gmail.com>
2016-10-17 09:16:27 -0400

    Breakup backend js and inline into pages.

--- server/auvsi_suas/static/auvsi_suas/pages/mission-list/mission-list-controller_test.js
+++ server/auvsi_suas/static/auvsi_suas/pages/mission-list/mission-list-controller_test.js
@@ -3,27 +3,36 @@
  */
 
 
-describe("MissionListCtrl controller", function() {
-    var backend, missionListCtrl;
+describe('MissionListCtrl controller', function() {
+    var httpBackend, missionListCtrl;
 
     beforeEach(module('auvsiSuasApp'));
 
-    beforeEach(inject(function($controller) {
-        backend = {missions: null};
-        missionListCtrl = $controller('MissionListCtrl', {Backend: backend});
+    beforeEach(inject(function($httpBackend, $controller, Backend) {
+        httpBackend = $httpBackend;
+        missionListCtrl = $controller('MissionListCtrl', {
+            'Backend': Backend
+        });
     }));
 
-    it("Shouldn't have missions with null list", function() {
+    it('Shouldnt have missions with null list', function() {
         expect(missionListCtrl.hasMissions()).toBe(false);
+        expect(missionListCtrl.getMissions()).toBe(null);
     });
 
-    it("Shouldn't have missions with empty list", function() {
-        backend.missions = [];
+    it('Shouldnt have missions with empty list', function() {
+        httpBackend.whenGET('/api/missions').respond([]);
+        httpBackend.flush();
         expect(missionListCtrl.hasMissions()).toBe(false);
+        expect(missionListCtrl.getMissions().length).toEqual(0);
     });
 
-    it("Should have missions with non-empty list", function() {
-        backend.missions = [{id: 1}];
+    it('Should have missions with non-empty list', function() {
+        var missions = [{id: 1}];
+        httpBackend.whenGET('/api/missions').respond(missions);
+        httpBackend.flush();
         expect(missionListCtrl.hasMissions()).toBe(true);
+        expect(missionListCtrl.getMissions().length).toEqual(1);
+        expect(missionListCtrl.getMissions()[0].toJSON()).toEqual(missions[0]);
     });
 });
