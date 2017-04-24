471f8b38cad68009478015485f6dc3ab162aba84
Yogisha Dixit <yad4@cornell.edu>
2017-04-23 19:52:25 -0700

    First pass stationary obstacle

--- server/auvsi_suas/models/stationary_obstacle.py
+++ server/auvsi_suas/models/stationary_obstacle.py
@@ -23,6 +23,35 @@ class StationaryObstacle(models.Model):
                                     str(self.cylinder_height),
                                     self.gps_position.__unicode__()))
 
+    def cylinder_collision(self, start_log, end_log, utm):
+        def get_projected(aerial_pos):
+            lat = aerial_pos.gps_position.latitude
+            lon = aerial_pos.gps_position.longitude
+            proj_x, proj_y = pyproj.transform(wgs84, utm, lon, lat)
+            return proj_x, proj_y
+
+        x1, y1 = get_projected(start_log.gps_position)
+        z1 = units.feet_to_meters(start_log.altitude_msl)
+        x2, y2 = get_projected(end_log.gps_position)
+        z2 = units.feet_to_meters(end_log.altitude_msl)
+
+        cx, cy = get_projected(self.gps_position)
+
+        m = (y2 - y1) / (x2 - x1)
+        b = y1 - m * x1
+
+        # x^2 + (mx + b)^2 = r^2
+        # x^2 + m^2x^2 + 2mxb + b^2 = r^2
+        # (m^2 + 1)x^2 + (2mb) x + (b^2 - r^2)
+        p = [m*m + 1, 2*m*b, b*b - self.cylinder_radius * self.cylinder_radius]
+        roots = np.roots(p)
+
+        for root in roots:
+            zcalc = ((root - x1) * (z2 - z1) / (x2 - x1)) + z1
+            if (zcalc > 0 and zcalc < self.cylinder_height):
+                return True
+
+
     def contains_pos(self, aerial_pos):
         """Whether the pos is contained within the obstacle.
 
@@ -52,9 +81,17 @@ class StationaryObstacle(models.Model):
             Whether a UAS telemetry log reported indicates a collision with the
             obstacle.
         """
-        for cur_log in uas_telemetry_logs:
-            if self.contains_pos(cur_log.uas_position):
-                return True
+        zone, north = distance.utm_zone(self.gps_position.latitude,
+                                        self.gps_position.longitude)
+        utm = distance.proj_utm(zone, north)
+        for i in range(0, len(uas_telemetry_logs)):
+            if i > 0:
+                if cylinder_collision(uas_telemetry_logs[i-1],
+                                      uas_telemetry_logs[i], utm):
+                    return True
+            else:
+                if self.contains_pos(cur_log.uas_position):
+                    return True
         return False
 
     def json(self):
