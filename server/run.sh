784b124d7121803a47e7f9d454b2f8a968bcc154
Phillip Tischler <pmtischler@gmail.com>
2017-07-10 08:04:19 -0400

    Create more granular health check.
    Use it to check for postgres health prior to trying to insert data into
    the postgres database. Use it instead of flaky and slower-than-needed
    sleep calls.

--- server/run.sh
+++ server/run.sh
@@ -3,8 +3,8 @@
 
 docker run -d --restart=unless-stopped --interactive --tty --publish 8000:80 --name interop-server auvsisuas/interop-server
 
-# Poll server up to 2 min for healthiness before proceeding.
-for i in {1..120};
+# Poll server up to 5 min for healthiness before proceeding.
+for i in {1..300};
 do
     docker inspect -f "{{.State.Health.Status}}" interop-server | grep "^healthy$" && exit 0 || sleep 1;
 done
