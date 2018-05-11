3943397558a060025813ff0b79e2907676b2f9d5
Phillip Tischler <pmtischler@gmail.com>
2018-05-11 09:00:49 -0400

    Remove NPM and frontend tests from CI.
    NPM / NodeJS has been too flaky for CI. These packages aren't needed for
    the prod use, just for testing the frontend. Will rely on manual local
    frontend tests instead.

--- server/test.sh
+++ server/test.sh
@@ -5,6 +5,4 @@ docker run -it auvsisuas/interop-server bash -c \
     "sudo service postgresql start && \
      sudo service memcached start && \
      cd /interop/server && \
-     python manage.py test && \
-     cd /interop/server/auvsi_suas/static/auvsi_suas && \
-     bash test_with_phantomjs.sh"
+     python manage.py test"
