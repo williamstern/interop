c6544433d2d3a3973e17edc0b550ba4332dcacb3
Phillip Tischler <pmtischler@gmail.com>
2018-12-02 16:18:35 -0500

    Deprecate WebGL map. Deprecate APIs. Cleanup UI.
    Eliminates the WebGL map to reduce maintenance burden. The upgraded KML
    views are more featureful and easier to maintain.
    
    Deprecates APIs which were only used in the WebGL map as they are no
    longer used.
    
    Cleans up the UI to contain the viewport. Resizes and rearranges
    components so it's easier to view.

--- server/auvsi_suas/frontend/pages/mission-dashboard-controller.js
+++ server/auvsi_suas/frontend/pages/mission-dashboard-controller.js
@@ -5,7 +5,6 @@
 
 /**
  * Controller for the Mission Dashboard page.
- * @param {!angular.$q} $q The promise service.
  * @param {!angular.Scope} $rootScope The root scope to listen for events.
  * @param {!angular.$routeParams} $routeParams The route parameter service.
  * @param {!angular.$interval} $interval The interval service.
@@ -15,32 +14,11 @@
  * @struct
  * @ngInject
  */
-MissionDashboardCtrl = function($q, $rootScope, $routeParams, $interval,
-                                Backend) {
-    /**
-     * @export {?Object} The mission data.
-     */
-    this.mission = null;
-
+MissionDashboardCtrl = function($rootScope, $routeParams, $interval, Backend) {
     /**
      * @export {?Object} The teams data.
      */
     this.teams = null;
-
-    /**
-     * @export {?Object} The obstacles data.
-     */
-    this.obstacles = null;
-
-    /**
-     * @private @const {?Object} The scene built by the map view, via template.
-     */
-    this.missionScene = null;
-
-    /**
-     * @private @const {!angular.$q} The promise service.
-     */
-    this.q_ = $q;
 
     /**
      * @private @const {!angular.$routeParams} The route params service.
@@ -57,19 +35,6 @@ MissionDashboardCtrl = function($q, $rootScope, $routeParams, $interval,
      */
     this.updateInterval_ = $interval(
             angular.bind(this, this.update_), 1000);
-
-    // Get the mission data.
-    this.backend_.missionResource.get({id: this.routeParams_['missionId']})
-        .$promise.then(angular.bind(this, this.setMission_));
-};
-
-
-/**
- * @return {?Object} The mission.
- * @export
- */
-MissionDashboardCtrl.prototype.getMission = function() {
-    return this.mission;
 };
 
 
@@ -85,27 +50,12 @@ MissionDashboardCtrl.prototype.isActive = function(team) {
 
 
 /**
- * @param {!Object} mission The mission data.
- */
-MissionDashboardCtrl.prototype.setMission_ = function(mission) {
-    this.mission = mission;
-};
-
-
-/**
  * Executes asynchronous updates for data.
  * @private
  */
 MissionDashboardCtrl.prototype.update_ = function() {
-    // Execute backend requests.
-    var requests = [];
-    requests.push(this.backend_.teamsResource.query({}).$promise
-        .then(angular.bind(this, this.setTeams_)));
-    requests.push(this.backend_.obstaclesResource.get({}).$promise
-        .then(angular.bind(this, this.setObstacles_)));
-
-    // Update display when requests finished.
-    this.q_.all(requests).then(angular.bind(this, this.rebuildMissionScene_));
+    this.backend_.teamsResource.query({}).$promise
+        .then(angular.bind(this, this.setTeams_));
 };
 
 
@@ -119,46 +69,8 @@ MissionDashboardCtrl.prototype.setTeams_ = function(teams) {
 };
 
 
-/**
- * Sets the obstacles.
- * @param {!Object} obstacles The obstacles to set.
- * @private
- */
-MissionDashboardCtrl.prototype.setObstacles_ = function(obstacles) {
-    this.obstacles = obstacles;
-};
-
-
-/**
- * Rebuild the mission scene using the backend data.
- * @private
- */
-MissionDashboardCtrl.prototype.rebuildMissionScene_ = function() {
-    if (!this.mission) {
-        return;
-    }
-
-    var telemetry = [];
-    if (this.teams) {
-        for (var i = 0; i < this.teams.length; i++) {
-            var team = this.teams[i];
-            if (team.telemetry && this.isActive(team)) {
-                telemetry.push(team.telemetry);
-            }
-        }
-    }
-
-    if (!!this.missionScene) {
-        // Rebuild scene with current mission.
-        this.missionScene.rebuildScene(
-                this.mission, this.obstacles, telemetry);
-    }
-};
-
-
 // Register controller with app.
 angular.module('auvsiSuasApp').controller('MissionDashboardCtrl', [
-    '$q',
     '$rootScope',
     '$routeParams',
     '$interval',
